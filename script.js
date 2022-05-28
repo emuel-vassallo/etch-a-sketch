91ef00c8392687a5d4d98576b70076a5d611a86d
emuel-vassallo <emuelvassallo15@gmail.com>
2022-05-28 16:57:01 +0200

    add border radius to grid

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function createGrid(sideSquaresAmount) {
   const gridContainer = document.querySelector('#grid-container');
   const totalSquaresAmount = Math.pow(sideSquaresAmount, 2);
+  const topRightSquareIndex = sideSquaresAmount - 1;
+  const bottomLeftSquareIndex = totalSquaresAmount - sideSquaresAmount;
+
   for (let i = 0; i < totalSquaresAmount; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-square');
+    if (i === topRightSquareIndex) div.setAttribute('id', 'top-right-square');
+    if (i === bottomLeftSquareIndex)
+      div.setAttribute('id', 'bottom-left-square');
     gridContainer.appendChild(div);
   }
 }
