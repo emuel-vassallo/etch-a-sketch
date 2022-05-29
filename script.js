4fc7ed78d94b92e6ae153de92e5e90088ca4fc08
emuel-vassallo <emuelvassallo15@gmail.com>
2022-05-29 13:57:21 +0200

    add functionality to change grid size

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function createGrid(sideSquaresAmount) {
+function createGrid(squaresPerSide) {
   const gridContainer = document.querySelector('#grid-container');
-  const totalSquaresAmount = Math.pow(sideSquaresAmount, 2);
-  const topRightSquareIndex = sideSquaresAmount - 1;
-  const bottomLeftSquareIndex = totalSquaresAmount - sideSquaresAmount;
+  const totalSquaresAmount = Math.pow(squaresPerSide, 2);
+  const topRightSquareIndex = squaresPerSide - 1;
+  const bottomLeftSquareIndex = totalSquaresAmount - squaresPerSide;
+
+  gridContainer.style.gridTemplateColumns = `repeat(${squaresPerSide}, minmax(0, 1fr))`;
 
   for (let i = 0; i < totalSquaresAmount; i++) {
     const div = document.createElement('div');
@@ -14,22 +16,27 @@ function createGrid(sideSquaresAmount) {
   }
 }
 
-function clearGrid() {
+function removAllGridSquares() {
   const squares = document.querySelectorAll('.grid-square');
   squares.forEach((square) => {
     square.remove();
   });
 }
 
-function changeColorDefault(squareElement) {
-  squareElement.classList.add('default-drawn-color');
-}
-
 function removeColorDefault(squareElement) {
   squareElement.classList.remove('default-drawn-color');
 }
 
-function changeHoveredSquareColor() {
+function changeColorDefault(squareElement) {
+  squareElement.classList.add('default-drawn-color');
+}
+
+function updateShownGridSize(squaresPerSide) {
+  const gridSizeTag = document.querySelector('#grid-size');
+  gridSizeTag.textContent = `${squaresPerSide} x ${squaresPerSide}`;
+}
+
+function changeHoveredSquaresColor() {
   const squares = document.querySelectorAll('.grid-square');
 
   squares.forEach((square) => {
@@ -51,12 +58,45 @@ function clearGridOnClick() {
   });
 }
 
-function showGridSize(squaresPerSide) {
-  const gridSizeTag = document.querySelector('#grid-size');
-  gridSizeTag.textContent = `${squaresPerSide} x ${squaresPerSide}`;
+function updateGrid(newSquaresPerSide) {
+  removAllGridSquares();
+  createGrid(newSquaresPerSide);
+  updateShownGridSize(newSquaresPerSide);
+  changeHoveredSquaresColor();
+  clearGridOnClick();
+}
+
+function getNewSquaresPerSide() {
+  let newSquaresPerSide = prompt(
+    'Enter the new amount of squares per side (1-100):'
+  );
+  newSquaresPerSide = parseInt(newSquaresPerSide);
+  newSquaresPerSide = Math.round(newSquaresPerSide);
+
+  const isInputInvalid =
+    newSquaresPerSide < 1 ||
+    newSquaresPerSide > 100 ||
+    isNaN(newSquaresPerSide);
+
+  if (isInputInvalid) return;
+  return newSquaresPerSide;
+}
+
+function changeGridSizeOnRequest() {
+  const changeGridSizeButton = document.querySelector('#change-grid-size');
+
+  changeGridSizeButton.addEventListener('click', () => {
+    const newSquaresPerSide = getNewSquaresPerSide();
+    const isNewSquaresPerSideValid = newSquaresPerSide !== undefined;
+
+    if (isNewSquaresPerSideValid) {
+      updateGrid(newSquaresPerSide);
+    }
+  });
 }
 
 createGrid(16);
-changeHoveredSquareColor();
+updateShownGridSize(16);
+changeHoveredSquaresColor();
 clearGridOnClick();
-showGridSize(16);
+changeGridSizeOnRequest();
